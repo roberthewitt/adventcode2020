22c5a63faf1b9c8250e8617038b6652adfd09fb0
Robert Hewitt <fuzzybear@gmail.com>
2020-12-03 21:43:45 +0000

    day 3
    added a can move query

--- day3.dart
+++ day3.dart
@@ -1,14 +1,38 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
+
+const tree = "#";
+
+class DataGrid {
+  int _hits = 0;
+  Point _location = new Point(0, 0);
+  List<List<String>> _column = [];
+
+  get location => _location;
+  get hits => _hits;
+
+  bool canMove(Point by) {
+    return (_location.y + by.y) < _column.length ;
+  }
+
+  void move(Point by) {}
+
+  void addRow(List<String> row) {
+    _column.add(row);
+  }
+}
 
 void main() {
-  int treesHit = 0;
+  var grid = new DataGrid();
   new File("inputData_day3.txt")
       .openRead()
       .map(utf8.decode)
       .transform(new LineSplitter())
-      .forEach((element) {})
-      .whenComplete(() {
-    print('trees hit: ${treesHit}');
+      .forEach((line) {
+    grid.addRow(line.split(""));
+  }).whenComplete(() {
+   
+    print('trees hit: ${grid.hits}');
   });
 }
--- test/day3_test.dart
+++ test/day3_test.dart
@@ -1,11 +1,32 @@
-import 'package:mockito/mockito.dart';
+import 'dart:math';
+
 import 'package:test/test.dart';
 
 import '../day3.dart';
 
-
 void main() {
-  group('day 3', () {
+  group('canMove', () {
+    test('try to move down by 1', () {
+      var grid = new DataGrid();
 
+      grid.addRow(["", "", "", ""]);
+      grid.addRow(["", "", "", ""]);
+
+      var result = grid.canMove(new Point(1, 1));
+
+      expect(result, equals(true),
+          reason: "we should be able to move by 1 down");
+    });
+
+    test('try to move down by 1 when only 1 row', () {
+      var grid = new DataGrid();
+
+      grid.addRow(["", "", "", ""]);
+
+      var result = grid.canMove(new Point(1, 1));
+
+      expect(result, equals(false),
+          reason: "we should not be able to move by 1 down");
+    });
   });
 }
