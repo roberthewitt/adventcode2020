2746f4716a5aba40623ba307ed3166f91d049475
RobertHewitt <robert.hewitt@fanduel.com>
2020-12-07 22:12:57 +0000

    reformatted to a larger line size

--- src/day7.dart
+++ src/day7.dart
@@ -19,11 +19,12 @@ Processor newProcessor() {
   List<String> lines = [];
   output.callback = lines.add;
 
-  bool bagContainsBag(Map<String, Iterable<BagContains>> bag, String search,
-          Iterable<BagContains> contents) =>
+  bool bagContainsBag(Map<String, Iterable<BagContains>> bag, String search, Iterable<BagContains> contents) =>
       contents.any((content) => content.bagName == search) ||
-      contents
-          .any((element) => bagContainsBag(bag, search, bag[element.bagName]));
+      contents.any((element) => bagContainsBag(bag, search, bag[element.bagName]));
+
+  int bagSize(Iterable<BagContains> contents, Map<String, Iterable<BagContains>> data) =>
+      contents.fold<int>(1, (old, bagContain) => old + bagContain.quantity * bagSize(data[bagContain.bagName], data));
 
   Map<String, Iterable<BagContains>> Function() bags =
       () => lines.fold<Map<String, Iterable<BagContains>>>({}, (acc, line) {
@@ -41,33 +42,21 @@ Processor newProcessor() {
           });
 
   output.pt1 = (rule) {
-    var b = bags();
-
-    return b.entries
-        .where((entry) => bagContainsBag(b, rule, entry.value))
-        .length;
+    var data = bags();
+    return data.entries.where((entry) => bagContainsBag(data, rule, entry.value)).length;
   };
 
   output.pt2 = (search) {
     var data = bags();
-    return bagSize(data[search], data) -1;
+    return bagSize(data[search], data) - 1;
   };
 
   return output;
 }
 
-int bagSize(Iterable<BagContains> contents,
-        Map<String, Iterable<BagContains>> data) =>
-    contents.fold<int>(
-        1,
-        (old, bagContain) =>
-            old +
-            bagContain.quantity * bagSize(data[bagContain.bagName], data));
-
 main() {
   var processor_testData = newProcessor();
-  readFileByLine("data/day7_testData.txt", processor_testData.callback,
-      onComplete: () {
+  readFileByLine("data/day7_testData.txt", processor_testData.callback, onComplete: () {
     print('<< test data result: ${processor_testData.pt1("shiny gold")}');
   });
 
@@ -77,15 +66,12 @@ main() {
   });
 
   var processor_testData2 = newProcessor();
-  readFileByLine("data/day7_testData2.txt", processor_testData2.callback,
-      onComplete: () {
-    print(
-        '<< test data PART 2 result: ${processor_testData2.pt2("shiny gold")}');
+  readFileByLine("data/day7_testData2.txt", processor_testData2.callback, onComplete: () {
+    print('<< test data PART 2 result: ${processor_testData2.pt2("shiny gold")}');
   });
 
   var processor_pt2 = newProcessor();
-  readFileByLine("data/day7.txt", processor_pt2.callback,
-      onComplete: () {
+  readFileByLine("data/day7.txt", processor_pt2.callback, onComplete: () {
     print('<< part 2 >> result: ${processor_pt2.pt2("shiny gold")}');
   });
 }
