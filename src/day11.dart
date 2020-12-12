8a33f82ee7977fb2e1c72550094df24b9a08e636
Robert Hewitt <fuzzybear@gmail.com>
2020-12-12 00:28:00 +0000

    day 11 pt 1 & 2 complete

--- src/day11.dart
+++ src/day11.dart
@@ -14,26 +14,35 @@ mixin RuleProcessor {
 
 int part1(List<List<String>> grid,
     {int iteration: 0, List<RuleProcessor> rules}) {
-  List<List<String>> newGrid = List.filled(grid.length, List(grid[0].length));
+  List<List<String>> newGrid = [];
+  for (var _column in grid) newGrid.add(List.filled(_column.length, null));
 
   bool valueFlipped = false;
   for (int y = 0; y < grid.length; y++) {
-    var row = grid[y];
-    for (int x = 0; x < row.length; x++) {
+    for (int x = 0; x < grid[y].length; x++) {
       var point = Point(x, y);
       var currentValue = grid[y][x];
-      newGrid[y][x] = rules.fold<String>(currentValue, (acc, rule) {
-        return (acc == currentValue) ? rule.processSpace(point, grid) : acc;
-      });
-      valueFlipped = valueFlipped ? true : newGrid[y][x] != currentValue;
+      var nextValue = rules.fold<String>(
+          currentValue,
+          (acc, rule) =>
+              (acc == currentValue) ? rule.processSpace(point, grid) : acc);
+      newGrid[y][x] = nextValue;
+      if (!valueFlipped) valueFlipped = currentValue != nextValue;
     }
   }
 
-  if (valueFlipped) return part1(newGrid, iteration: iteration +1, rules: rules);
+  if (valueFlipped) {
+    log("iteration ${iteration} => ${iteration + 1}");
+    for (var column = 0; column < grid.length; column++) {
+      log("${grid[column]} => ${newGrid[column]}");
+    }
+    return part1(newGrid, iteration: iteration + 1, rules: rules);
+  }
 
-  print("finished with: $iteration");
-  newGrid.forEach(print);
-  return 0;
+  return newGrid.fold<int>(
+      0,
+      (acc, rows) =>
+          acc + rows.fold<int>(0, (a, seat) => seat == "#" ? a + 1 : a));
 }
 
 Processor day11() {
@@ -43,9 +52,11 @@ Processor day11() {
 
   output.callback = (line) => lines.add(line.split(""));
 
-  output.pt1 = () => part1([...lines], rules: [EmptySeatRule()]);
+  output.pt1 = () => part1([...lines],
+      iteration: 0, rules: [EmptySeatRulePt1(), OccupiedSeatRulePt1()]);
 
-  output.pt2 = () => 0;
+  output.pt2 = () => part1([...lines],
+      iteration: 0, rules: [EmptySeatRulePt2(), OccupiedSeatRulePt2()]);
 
   return output;
 }
@@ -71,22 +82,74 @@ main() {
   });
 }
 
-class EmptySeatRule with RuleProcessor {
+class EmptySeatRulePt1 with RuleProcessor {
+  @override
+  String processSpace(Point<num> coord, List<List<String>> grid) =>
+      (grid[coord.y][coord.x] == "L" &&
+              adjacentTo(coord, grid)
+                      .where((a) => grid[a.y][a.x] == "#")
+                      .length ==
+                  0)
+          ? "#"
+          : grid[coord.y][coord.x];
+}
+
+
+class EmptySeatRulePt2 with RuleProcessor {
   @override
   String processSpace(Point<num> coord, List<List<String>> grid) {
-    var seat = grid[coord.y][coord.x];
-    var adjacent = adjacentSeats(coord, grid);
-    if (seat == "L") {
-      return adjacent.where((a) => grid[a.y][a.x] == "#").length == 0
-          ? "#"
-          : seat;
-    }
-    return seat;
+    var current = grid[coord.y][coord.x];
+    if (current != 'L') return current;
+    return lineOfSightVectors.fold(0, (acc, vector) {
+              var lastUsed = Point(coord.x, coord.y);
+              for (int i = 0; i < grid.length + 1; i++){
+                lastUsed = Point(lastUsed.x + vector.x, lastUsed.y + vector.y);
+                if (lastUsed.x >= grid[0].length || lastUsed.x < 0) return acc;
+                if (lastUsed.y >= grid.length || lastUsed.y < 0) return acc;
+                if (grid[lastUsed.y][lastUsed.x] == "#") return acc +1;
+                if (grid[lastUsed.y][lastUsed.x] == "L") return acc;
+              }
+              return acc;
+            }) == 0 ? "#" : "L";
   }
 }
 
-List<Point> adjacentSeats(Point source, List<List<String>> grid) {
-  int maxX = grid[0].length - 1;
+class OccupiedSeatRulePt1 with RuleProcessor {
+  @override
+  String processSpace(Point<num> coord, List<List<String>> grid) =>
+      (grid[coord.y][coord.x] == "#" &&
+              adjacentTo(coord, grid)
+                      .where((a) => grid[a.y][a.x] == "#")
+                      .length >=
+                  4)
+          ? "L"
+          : grid[coord.y][coord.x];
+}
+
+class OccupiedSeatRulePt2 with RuleProcessor {
+  @override
+  String processSpace(Point<num> coord, List<List<String>> grid) {
+    var current = grid[coord.y][coord.x];
+    if (current != '#') return current;
+    return lineOfSightVectors.fold(0, (acc, vector) {
+      var lastUsed = Point(coord.x, coord.y);
+      for (int i = 0; i < grid.length + 1; i++){
+        lastUsed = Point(lastUsed.x + vector.x, lastUsed.y + vector.y);
+        if (lastUsed.x >= grid[0].length || lastUsed.x < 0) return acc;
+        if (lastUsed.y >= grid.length || lastUsed.y < 0) return acc;
+        if (grid[lastUsed.y][lastUsed.x] == "#") return acc +1;
+        if (grid[lastUsed.y][lastUsed.x] == "L") return acc;
+      }
+      return acc;
+    }) >=
+        5
+        ? "L"
+        : "#";
+  }
+}
+
+List<Point> adjacentTo(Point source, List<List<String>> grid) {
+  int maxX = grid[source.y].length - 1;
   int maxY = grid.length - 1;
   Map<int, List<int>> modifiers = {
     -1: [-1, 0, 1],
@@ -97,9 +160,25 @@ List<Point> adjacentSeats(Point source, List<List<String>> grid) {
     var column = source.y + entry.key;
     entry.value.forEach((rowMod) {
       var row = source.x + rowMod;
+      if (source.x == row && source.y == column) return acc;
       if (row >= 0 && column >= 0 && row <= maxX && column <= maxY)
-        acc.add(Point(column, row));
+        acc.add(Point(row, column));
     });
     return acc;
   });
 }
+
+void log(dynamic any) {
+  if (false) print(any);
+}
+
+const lineOfSightVectors = [
+  Point(-1,-1), // NW
+  Point(0, -1), // N
+  Point(1, -1), // NE
+  Point(-1, 0), // W
+  Point(1, 0), // E
+  Point(-1, 1), // SW
+  Point(0, 1), // S
+  Point(1, 1), // SE
+];
--- test/day11_test.dart
+++ test/day11_test.dart
@@ -8,26 +8,125 @@ main() {
       test("flips all empty seats", () {
         var processor = day11();
 
-        // 5x5 grid of seats
+        // 3x3 grid of seats
         var lines = [
           "LLL",
           "LLL",
           "LLL",
         ];
         lines.forEach(processor.callback);
+        // should result in
+        // [#, L, #]
+        // [L, L, L]
+        // [#, L, #]
 
-        expect(processor.pt1(), equals(0));
+        expect(processor.pt1(), equals(4));
+      });
+
+      test("4x4 empty", () {
+        var processor = day11();
+
+        var lines = [
+          "LLLL",
+          "LLLL",
+          "LLLL",
+          "LLLL",
+        ];
+        lines.forEach(processor.callback);
+        // should result in
+        // [#, L, L, #]
+        // [L, L, L, L]
+        // [L, L, L, L]
+        // [#, L, L, #]
+
+        expect(processor.pt1(), equals(4));
+      });
+
+      test("5x5 empty", () {
+        var processor = day11();
+
+        var lines = [
+          "LLLLL",
+          "LLLLL",
+          "LLLLL",
+          "LLLLL",
+          "LLLLL",
+        ];
+        lines.forEach(processor.callback);
+        // should result in
+        // [#, L, #, L, #]
+        // [L, L, L, L, L]
+        // [#, L, #, L, #]
+        // [L, L, L, L, L]
+        // [#, L, #, L, #]
+
+        expect(processor.pt1(), equals(9));
+      });
+
+      test("ignores .", () {
+        var processor = day11();
+
+        var lines = [
+          ".....",
+          "LLLLL",
+          "LLLLL",
+          "LLLLL",
+          "LLLLL",
+          "LLLLL",
+        ];
+        lines.forEach(processor.callback);
+        // should result in
+        // [., ., ., ., .]
+        // [#, L, #, L, #]
+        // [L, L, L, L, L]
+        // [#, L, #, L, #]
+        // [L, L, L, L, L]
+        // [#, L, #, L, #]
+
+        expect(processor.pt1(), equals(9));
+      });
+
+      test("test data", () {
+        var processor = day11();
+
+        var lines = [
+          "L.LL.LL.LL",
+          "LLLLLLL.LL",
+          "L.L.L..L..",
+          "LLLL.LL.LL",
+          "L.LL.LL.LL",
+          "L.LLLLL.LL",
+          "..L.L.....",
+          "LLLLLLLLLL",
+          "L.LLLLLL.L",
+          "L.LLLLL.LL"
+        ];
+        lines.forEach(processor.callback);
+
+        expect(processor.pt1(), equals(37));
       });
     });
 
     group("part 2", () {
-      test("dummy", () {
+
+      test("test data", () {
         var processor = day11();
 
-        var lines = [""];
+        var lines = [
+          "L.LL.LL.LL",
+          "LLLLLLL.LL",
+          "L.L.L..L..",
+          "LLLL.LL.LL",
+          "L.LL.LL.LL",
+          "L.LLLLL.LL",
+          "..L.L.....",
+          "LLLLLLLLLL",
+          "L.LLLLLL.L",
+          "L.LLLLL.LL"
+        ];
         lines.forEach(processor.callback);
 
-        expect(processor.pt2(), equals(0));
+        expect(processor.pt2(), equals(26));
       });
     });
   });
