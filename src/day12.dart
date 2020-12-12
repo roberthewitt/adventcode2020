5fabae03dca6e89832473998b3ce4aef230b7009
RobertHewitt <robert.hewitt@fanduel.com>
2020-12-12 11:49:32 +0000

    day 12 part 1
    some initial set up and a first test around movement

---
+++ src/day12.dart
@@ -0,0 +1,90 @@
+import 'dart:math';
+
+import '../utils.dart';
+
+class Processor {
+  Function(String) callback;
+  int Function() pt1;
+  int Function() pt2;
+}
+
+Processor day12() {
+  var output = Processor();
+
+  List<String> lines = [];
+
+  output.callback = lines.add;
+
+  output.pt1 = () => part1Solution([...lines]);
+
+  output.pt2 = () => 0;
+
+  return output;
+}
+
+main() {
+  var day = 12;
+  var testData = "data/day${day}_testData.txt";
+  var realData = "data/day${day}.txt";
+
+  var processor_testData = day12();
+  readFileByLine(testData, processor_testData.callback, solve: () {
+    print('<< day $day pt 1 >> ${processor_testData.pt1()}');
+  }, onComplete: () {
+    var processor_pt1 = day12();
+    readFileByLine(realData, processor_pt1.callback, solve: () {
+      print('<< day $day pt 1 >> ${processor_pt1.pt1()}');
+    }, onComplete: () {
+      var processor_pt2 = day12();
+      readFileByLine(realData, processor_pt2.callback, solve: () {
+        print('<< day $day pt 2 >> ${processor_pt2.pt2()}');
+      });
+    });
+  });
+}
+
+const FORWARDS = "F";
+const BACKWARDS = "B";
+const NORTH = "N";
+const EAST = "E";
+const SOUTH = "S";
+const WEST = "W";
+const LEFT = "L";
+const RIGHT = "R";
+
+int part1Solution(List<String> orders) {
+  int facing = 180;
+
+  var newLocation = orders.fold<Point>(Point(0, 0), (acc, order) {
+    String direction = order.substring(0, 1);
+    int magnitude = int.parse(order.substring(1, order.length));
+
+    if (direction == FORWARDS) direction = translateDirection(facing);
+    if (direction == BACKWARDS) direction = translateDirection(facing, reverse: false);
+
+    if (direction == NORTH) acc += Point(0, magnitude);
+    if (direction == SOUTH) acc += Point(0, -magnitude);
+    if (direction == EAST) acc += Point(-magnitude, 0);
+    if (direction == WEST) acc += Point(magnitude, 0);
+    if (direction == LEFT) facing -= magnitude;
+    if (direction == RIGHT) facing += magnitude;
+
+    return acc;
+  });
+
+  var manhattan = newLocation.x.abs() + newLocation.y.abs();
+
+  print("New Location : $newLocation");
+
+  return manhattan;
+}
+
+String translateDirection(int facing, {bool reverse: true}) {
+  if (facing == 0) return reverse ? WEST : EAST;
+  if (facing == 90) return reverse ? SOUTH : NORTH;
+  if (facing == 180) return reverse ? EAST : WEST;
+  if (facing == 270)
+    return reverse ? NORTH : SOUTH;
+  else
+    throw StateError("Unknown facing direction $facing");
+}
---
+++ test/day12_test.dart
@@ -0,0 +1,50 @@
+import 'package:test/test.dart';
+
+import '../src/day12.dart';
+
+main() {
+  group("day 11", () {
+    group("part 1", () {
+
+      test("moving forwards 5, then east 5 should equal 10", () {
+        var processor = day12();
+
+        var lines = [
+          "F5",
+          "E5",
+        ];
+        lines.forEach(processor.callback);
+
+        expect(processor.pt1(), equals(10));
+
+      });
+
+
+      test("test data", () {
+        var processor = day12();
+
+        var lines = [
+          "F10",
+          "N3",
+          "F7",
+          "R90",
+          "F11",
+        ];
+        lines.forEach(processor.callback);
+
+        expect(processor.pt1(), equals(25));
+      });
+    });
+
+    group("part 2", () {
+      test("test data", () {
+        var processor = day12();
+
+        var lines = [];
+        lines.forEach(processor.callback);
+
+        expect(processor.pt2(), equals(0));
+      });
+    });
+  });
+}
