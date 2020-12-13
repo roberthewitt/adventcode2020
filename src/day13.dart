8c303524d758cd68889c4401862bc913b706410f
RobertHewitt <robert.hewitt@fanduel.com>
2020-12-13 15:34:50 +0000

    day 13 part 1 complete

--- src/day13.dart
+++ src/day13.dart
@@ -9,11 +9,34 @@ class Processor {
 Processor day13() {
   var output = Processor();
 
-  List<String> lines = [];
+  Iterable<int> busIds = [];
+  int departureTimestamp = 0;
 
-  output.callback = lines.add;
+  output.callback = (line) {
+    if (departureTimestamp == 0) {
+      departureTimestamp = int.parse(line);
+    } else {
+      busIds = line.split(",").where((e) => e != "x").map(int.parse);
+    }
+  };
 
-  output.pt1 = () => 0;
+  output.pt1 = () {
+    var quickestTime = busIds
+        .fold<Map<int, int>>({}, (acc, busId) {
+          acc[busId] = busTimeAfter(busId: busId, target: departureTimestamp, current: 0);
+          return acc;
+        })
+        .entries
+        .fold<List<int>>([], (acc, entry) {
+          if (acc.isEmpty) acc = [entry.key, entry.value];
+          if (entry.value < acc[1]) acc = [entry.key, entry.value];
+          return acc;
+        });
+
+    print(quickestTime);
+
+    return (quickestTime[1] - departureTimestamp) * quickestTime[0];
+  };
 
   output.pt2 = () => 0;
 
@@ -41,3 +64,11 @@ main() {
   });
 }
 
+int busTimeAfter({
+  int busId,
+  int target,
+  int current,
+}) {
+  while (current < target) current += busId;
+  return current;
+}
--- test/day13_test.dart
+++ test/day13_test.dart
@@ -5,25 +5,64 @@ import '../src/day13.dart';
 main() {
   group("day 13", () {
     group("part 1", () {
-      test("stub", () {
+      // test("stub", () {
+      //   var processor = day13();
+      //
+      //   var lines = [];
+      //   lines.forEach(processor.callback);
+      //
+      //   expect(processor.pt1(), equals(0));
+      // });
+
+      test("test data", () {
         var processor = day13();
 
-        var lines = [];
+        var lines = [
+          "939",
+          "7,13,x,x,59,x,31,19",
+        ];
         lines.forEach(processor.callback);
 
-        expect(processor.pt1(), equals(0));
+        expect(processor.pt1(), equals(5 * 59));
+      });
+
+      test("can work out departure time of single bus", () {
+        var processor = day13();
+
+        var lines = ["10", "12"];
+        lines.forEach(processor.callback);
+
+        expect(processor.pt1(), equals(2 * 12));
+      });
+
+      test("can work out departure time of multiple bus", () {
+        var processor = day13();
+
+        var lines = ["10", "12,11"];
+        lines.forEach(processor.callback);
+
+        expect(processor.pt1(), equals(1 * 11));
+      });
+
+      test("ignores x's in the bus ids", () {
+        var processor = day13();
+
+        var lines = ["10", "12,x,11"];
+        lines.forEach(processor.callback);
+
+        expect(processor.pt1(), equals(1 * 11));
       });
     });
 
     group("part 2", () {
-      test("stub", () {
-        var processor = day13();
-
-        var lines = [];
-        lines.forEach(processor.callback);
-
-        expect(processor.pt2(), equals(0));
-      });
+      // test("stub", () {
+      //   var processor = day13();
+      //
+      //   var lines = [];
+      //   lines.forEach(processor.callback);
+      //
+      //   expect(processor.pt2(), equals(0));
+      // });
     });
   });
 }
