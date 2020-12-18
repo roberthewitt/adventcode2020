946164e744074b82d4db9217da71dddb66b32a9e
RobertHewitt <robert.hewitt@fanduel.com>
2020-12-18 15:26:37 +0000

    day 14 pt 1 complete

--- src/day14.dart
+++ src/day14.dart
@@ -14,31 +14,38 @@ Processor day14() {
   output.callback = input.add;
 
   output.pt1 = () {
-    // String mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-    // eg. all X would translate to nulls in all entries
     List<dynamic> masks = List(36);
     Map<int, int> memory = {};
 
     input.forEach((line) {
       if (line.startsWith("mask")) {
-        for (int i = 0; i < masks.length ; i++) masks[i] = null;
         var mask = line.split("mask = ")[1].trim();
-        print ("line processed as $mask");
         for (int i = 0; i < mask.length; i++) {
-          var value = mask.substring(i, i+1);
+          var value = mask.substring(i, i + 1);
           if (value == "1") masks[i] = 1;
+          if (value == "X") masks[i] = "X";
           if (value == "0") masks[i] = 0;
         }
       } else {
-        var stopIndex = line.indexOf("]");
-        int memoryAddress = int.parse(line.substring(4, stopIndex));
-        int value = int.parse(line.substring(line.indexOf("=") +1).trim());
-        print('do a memory thing with $memoryAddress $value');
+        int memoryAddress = int.parse(line.substring(4, line.indexOf("]")));
+        int number = int.parse(line.substring(line.indexOf("=") + 1).trim());
+        var binaryNumber = number.toRadixString(2).trim().padLeft(36, "0");
 
+        var newValue = applyMask(binaryNumber, masks);
+
+        var newNumber = int.parse(newValue, radix: 2);
+
+        memory[memoryAddress] = newNumber;
+
+        log('memory index $memoryAddress'
+            '\nvalue  :$binaryNumber '
+            '\nmask   :${maskToString(masks)} '
+            '\nresult :$newValue '
+            '\ndecimal:$newNumber');
       }
     });
 
-    return 0;
+    return memory.values.fold(0, (acc, val) => acc + val);
   };
 
   output.pt2 = () {
@@ -67,4 +74,37 @@ main() {
       });
     });
   });
+}
+
+String maskToString(List<dynamic> masks) => masks.fold("", (acc, e) => acc + e.toString());
+
+String applyMask(String binaryString, List<dynamic> masks) {
+  var a = binaryString.split("");
+
+  var result = "";
+  for (int i = 0; i < a.length; i++) {
+    var maskedBit = masks[i];
+    switch (maskedBit) {
+      case 1:
+        {
+          result += "1";
+          break;
+        }
+      case 0:
+        {
+          result += "0";
+          break;
+        }
+      default:
+        {
+          result += a[i];
+        }
+    }
+  }
+
+  return result;
+}
+
+void log(dynamic any) {
+  if (false) print(any);
 }
